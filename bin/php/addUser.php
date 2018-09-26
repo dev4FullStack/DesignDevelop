659ff09519749f2cceb6d04220ac959e835ada5a
Harold Payet <webpostaction@gmail.com>
2018-09-26 09:06:24 +0200

    feat:ajout de fonctionnalités PHP pour notre website file:addUser.php->core.php

--- bin/php/addUser.php
+++ bin/php/addUser.php
@@ -1,12 +1,12 @@
 
 <div class="userContent">
-	<form action="core.php"  method="POST">
-		<input type="text" name="user">
-		<input type="text" name="user">
-		<input type="text" name="user">
-		<input type="text" name="user">
-		<input type="text" name="user">
-		<input type="text" name="user">
+	<form action="bin/core.php?posData=addUser"  method="POST">
+		<input type="text" name="user" value="Entrez votre nom       ">
+		<input type="text" name="user" value="Entrez votre prenom    ">
+		<input type="text" name="user" value="Entrez votre e-mail    ">
+		<input type="text" name="user" value="Numero de téléphone    ">
+		<input type="text" name="user" value="Votre mot de passe     ">
+		<input type="text" name="user" value="Confirmez mot de passe ">
 
 
 		<input type="submit" value="Envoyer" name="userOk">
