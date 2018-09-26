437f7b203a635d380b078ded09a8b60ff1e16b9b
Harold Payet <webpostaction@gmail.com>
2018-09-26 09:20:59 +0200

    feat:ajout de variable au formulaire pour les passer au core.php

--- bin/php/addUser.php
+++ bin/php/addUser.php
@@ -1,12 +1,12 @@
 
 <div class="userContent">
-	<form action="bin/core.php?posData=addUser"  method="POST">
-		<input type="text" name="user" value="Entrez votre nom       ">
-		<input type="text" name="user" value="Entrez votre prenom    ">
-		<input type="text" name="user" value="Entrez votre e-mail    ">
-		<input type="text" name="user" value="Numero de téléphone    ">
-		<input type="text" name="user" value="Votre mot de passe     ">
-		<input type="text" name="user" value="Confirmez mot de passe ">
+	<form action="bin/php/core.php?posData=addUser"  method="POST">
+		<input type="text" name="userFName" value="Entrez votre nom       ">
+		<input type="text" name="userLName" value="Entrez votre prenom    ">
+		<input type="text" name="userEmail" value="Entrez votre e-mail    ">
+		<input type="text" name="userTel" value="Numero de téléphone    ">
+		<input type="text" name="userPwd1" value="Votre mot de passe     ">
+		<input type="text" name="userPwd2" value="Confirmez mot de passe ">
 
 
 		<input type="submit" value="Envoyer" name="userOk">
--- bin/php/core.php
+++ bin/php/core.php
@@ -1,2 +1,14 @@
 <?php
+	
+	$__main__ = 'starter';
+
+
+	if (isset($_GET)){
+		if($_GET['posData']){
+			var_dump($_POST);
+		}
+	}
+	
+
+
 ?>
